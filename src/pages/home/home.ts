a2039340ba31f34af5d301047e4a8b5bb006d5cb
Lucas Laet <lucas.laetlira@gmail.com>
2021-03-27 23:33:04 -0300

    Login info DTO created

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, MenuController, NavController } from 'ionic-angular';
+import {CredentialsDTO} from '../../models/credentials.dto';
 
 @IonicPage()
 @Component({
@@ -8,18 +9,24 @@ import { IonicPage, MenuController, NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  credentials : CredentialsDTO = {
+    email: '',
+    password: ''
+  };
+
   constructor(public navCtrl: NavController, public menu: MenuController) {
   }
 
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
-  
+
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
   }
 
   login(){
+    console.log(this.credentials);
     this.navCtrl.setRoot('CategoriesPage');
   }
 
