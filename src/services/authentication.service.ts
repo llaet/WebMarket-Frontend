ba8367d1322009db841bd8e838ee4e00fdd4142c
Lucas Laet <lucas.laetlira@gmail.com>
2021-06-04 17:29:27 -0300

    Captura Email Autenticação

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelper } from 'angular2-jwt';
 import { API_CONFIG } from '../config/api.config';
 import { CredentialsDTO } from '../models/credentials.dto';
 import { LocalUser } from '../models/local_user';
@@ -7,6 +8,8 @@ import { StorageService } from './storage.service';
 
 @Injectable()
 export class AuthenticationService {
+
+  jwtHelper: JwtHelper = new JwtHelper();
 
   constructor(public http: HttpClient, public storage: StorageService) {
   }
@@ -24,7 +27,8 @@ export class AuthenticationService {
   successfulLogin(authorizationValue: string) {
     let webToken = authorizationValue.substring(7);
     let user: LocalUser = {
-      token: webToken
+      token: webToken,
+      email: this.jwtHelper.decodeToken(webToken).sub
     };
     this.storage.setLocalUser(user);
   }
